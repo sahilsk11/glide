59bd045b9d2293309ed8769428a78f9bf6e506ad
Sahil Kapur <sahilkapur.a@gmail.com>
2020-12-29 23:29:59 -0800

    add flask endpoint

---
+++ src/endpoint.py
@@ -0,0 +1,21 @@
+import flask
+
+app = flask.Flask(__name__)
+
+@app.route("/")
+def healthcheck():
+  return flask.jsonify({"status": 200, "message": "application is running"})
+
+@app.route("/postResume", methods=['POST'])
+def accept_resume():
+  if authenticate(flask.request.json):
+    # save resume as file
+    # parse resume and get results
+    return flask.jsonify({"success": True})
+  return flask.jsonify({"code": 403, "message": "Invalid credentials"})
+
+def authenticate(data):
+  return True
+
+if __name__ == "__main__":
+  app.run(debug=True)
