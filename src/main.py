01ba741500caf88a1f228da0eec5e3859f647bd6
Saaniya Kapur <saaniya2@illinois.edu>
2021-01-02 02:00:53 -0800

    saaniya main

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 from resume_converter import get_cleaned_resume_text, resume_to_dict
 import json 
+from ruleset import checklist, is_resume_pdf, is_resume_scannable, scan_resume, calculate_points
+import endpoint
+
 
 def calculate_points(cleaned_text):
   f = open("points.json")
@@ -20,5 +23,11 @@ def interpret_points(points):
 if __name__ == "__main__":
   resume_text = get_cleaned_resume_text("sahil_kapur_resume.pdf")
   points = calculate_points(resume_text)
-  print(interpret_points(points), "("+str(points)+" points)")
-  print(resume_to_dict('sahil_kapur_resume.pdf'))
+  #print(interpret_points(points), "("+str(points)+" points)")
+  #print(json.dumps(resume_to_dict('Kapur_Saaniya.pdf')))
+  print(checklist("Kapur_Saaniya.pdf"))
+  print(is_resume_scannable("Kapur_Saaniya.pdf"))
+  print(scan_resume("Kapur_Saaniya.pdf"))
+  endpoint.pdf_to_png("Kapur_Saaniya.pdf")
+  
+  
