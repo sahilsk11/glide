5b57330c944d088bb0f217d9111007cdb24e2890
Saaniya Kapur <saaniya2@illinois.edu>
2021-01-02 01:58:25 -0800

    pdf_png function

--- src/endpoint.py
+++ src/endpoint.py
@@ -1,6 +1,8 @@
 import flask
 import os
 import ruleset
+import PyPDF2
+from PIL import Image
 
 app = flask.Flask(__name__)
 from flask_cors import CORS
@@ -30,6 +32,19 @@ def parse_resume():
 
 def authenticate(data):
   return True
+
+def pdf_to_png(filename):
+  if __name__ == '__main__':
+    input1 = PyPDF2.PdfFileReader(open(filename, "rb"))
+    page0 = input1.getPage(0)
+    xObject = page0['/Resources']['/XObject'].getObject()
+
+    for obj in xObject:
+      if xObject[obj]['/Filter'] == '/DCTDecode':
+        img = open(obj[1:] + ".jpg", "wb")
+        img.write(data)
+        img.save(filename + ".thumbnail", "JPEG")
+        img.close()
 
 if __name__ == "__main__":
   app.run(debug=True)
