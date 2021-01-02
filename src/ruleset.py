5797aa460c92ae94d370524702c69e81cb422467
Saaniya Kapur <saaniya2@illinois.edu>
2021-01-01 22:01:27 -0800

    adding returned list

--- src/ruleset.py
+++ src/ruleset.py
@@ -14,89 +14,47 @@ def scan_resume(filename):
 
 def checklist(filename):
     flag = 0
-    flag_1 = 0
-    flag_2 = 0
-    flag_3 = 0
-    flag_4 = 0
-    flag_5 = 0
-    flag_6 = 0
-    flag_7 = 0
-    flag_8 = 0
-    flag_9 = 0
 
     elements = []
     d = resume_to_dict(filename, path="saved-resumes/")
 
     if d.get("names") == None:
-        flag_1 = flag_1 + 1
-        print("You are missing your name!")                     #checks name
+        elements.append("Name")                     #checks name
     
     if d.get("emails") == None:
-        flag_2 = flag_2 + 1
-        print("You are missing your email!")                    #checks email
+        elements.append("Email")                    #checks email
     
     if d.get("phones") == None:
-        flag_3 = flag_3 + 1
-        print("You are missing your phone number!")             #checks phone number
+        elements.append("Phone Number")             #checks phone number
     
     for add in d["links"]:
         if add.get("domain") == "linkedin.com":
             flag = flag + 1
     if flag == 0:
-        print("You are missing your Linkedin account!")             #checks linkedin account 
+        elements.append("Linkedin")            #checks linkedin account 
 
     for edu in d["schools"]:
         if edu.get("degree") == None:
-            flag_4 = flag_4 + 1
-            print("You are missing your degree")                #checks degree
+            elements.append("Degree")               #checks degree
         if edu.get("gpa") == None:
-            flag_5 = flag_5 + 1
-            print("You are missing your GPA")                   #checks GPA
+            elements.append("GPA")                  #checks GPA
 
     for time in d["positions"]:                                 #checks dates 
         if time.get("isCurrent") != None:
             if time.get("start").get("year") == None:
-                flag_6 = flag_6 + 1
-                print("You are missing a start date year")
+                elements.append("Start Year")
             if time.get("start").get("month") == None:
-                flag_7 = flag_7 + 1
-                print("You are missing a start date month")
+                elements.append("Start Month")
         else:
             if time.get("start").get("year") == None:
-                flag_6 = flag_6 + 1
-                print("You are missing a start date year")
+                elements.append("Start Year")
             if time.get("start").get("month") == None:
-                flag_7 = flag_7 + 1
-                print("You are missing a start date month")
+                elements.append("Start Month")
             if time.get("end").get("year") == None:
-                flag_8 = flag_8 + 1
-                print("You are missing an end date year")
-            if time.get("end").get("year") == None:
-                flag_9 = flag_9 + 1
-                print("You are missing an end date month")
+                elements.append("End Year")
+            if time.get("end").get("month") == None:
+                elements.append("End Month")
     
-    if flag == 0:
-        elements.append("Linkedin")
-    if flag_1 != 0:
-        elements.append("Name")
-    if flag_2 != 0:
-        elements.append("Email")
-    if flag_3 != 0:
-        elements.append("Phone Number")
-    if flag_4 != 0:
-        elements.append("Degree")
-    if flag_5 != 0:
-        elements.append("GPA")
-    if flag_6 != 0:
-        elements.append("Start Year")
-    if flag_7 != 0:
-        elements.append("Start Month")
-    if flag_8 != 0:
-        elements.append("End Year")
-    if flag_9 != 0:
-        elements.append("End Month")
-   
-
     return elements
 
 
