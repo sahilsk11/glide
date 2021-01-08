8378dfc529399f483dbd4edec67a6e5a30af2745
Saaniya Kapur <saaniya2@illinois.edu>
2021-01-08 11:47:37 -0800

    update

--- src/main.py
+++ src/main.py
@@ -21,12 +21,12 @@ def interpret_points(points):
   return "you in homie"
 
 if __name__ == "__main__":
-  resume_name = "sahil_kapur_resume.pdf"
+  resume_name = "Sameer_Kapur_Resume.pdf"
   #s = get_cleaned_resume_text(resume_name)
   #print(d)
   d = resume_to_dict(resume_name)
+  print(ruleset.checklist(resume_name,d))
   print(ruleset.scan_resume(resume_name, d))
-  #print(ruleset.scan_resume(resume_name, d))
   #print(s)
 
   
