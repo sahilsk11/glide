b36a594cf300339ddd9e52c065f33d6d5ac14322
Sahil Kapur <sahilkapur.a@gmail.com>
2021-01-09 21:28:35 -0800

    fix poppler route

--- src/endpoint.py
+++ src/endpoint.py
@@ -10,9 +10,10 @@ import random
 import re
 import sys
 import traceback
+import logging
 
-# if '/usr/bin' not in os.environ:
-#   os.environ['PATH'] = '/usr/bin'
+if '/usr/bin' not in os.environ:
+  os.environ['PATH'] = '/usr/bin'
 
 app = flask.Flask(__name__)
 from flask_cors import CORS
@@ -55,7 +56,7 @@ def parse_resume():
       new_filename = generate_filename(filename)
       rename_file(filename, new_filename)
     except Exception as e:
-      print(traceback.print_exc())
+      logging.exception(e)
       return flask.jsonify({"success": False, "message": "There was an error in the request"})
     save_resume_to_db(
       original_filename,
