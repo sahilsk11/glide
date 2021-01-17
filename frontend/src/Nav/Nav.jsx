bdf8b5474c35c241205a5710252d0966a2b47474
Sahil Kapur <sahilkapur.a@gmail.com>
2021-01-16 22:38:56 -0800

    add feedback link

--- frontend/src/Nav/Nav.jsx
+++ frontend/src/Nav/Nav.jsx
@@ -13,7 +13,7 @@ export default function Nav({ useYellowBackground }) {
         <Icon />
       </div>
       <div className="nav-email-signup-wrapper">
-        {/* <EmailSignup /> */}
+        <a href="#" className="glide-link nav-feedback" target="_blank">Feedback</a>
       </div>
     </div>
   );
