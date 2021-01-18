5c8cc6f6a160f4835daae8d33f047da20f816cbb
Sahil Kapur <sahilkapur.a@gmail.com>
2021-01-17 20:09:50 -0800

    update contact on error page

--- frontend/src/Error/Error.jsx
+++ frontend/src/Error/Error.jsx
@@ -11,7 +11,7 @@ export default function Error() {
         <div className="error-content">
           <img alt="" src="./img/yarn.svg" className="error-icon" />
           <h1 className="error-title">Aw, snap.</h1>
-          <p className="error-subtitle">We ran into an error while processing your resume. Try again, or contact us at support@glideresume.com</p>
+          <p className="error-subtitle">We ran into an error while processing your resume. Try again, or contact us by dropping feedback <a className="glide-link" href="https://airtable.com/shr2dgrLZTvXliBD6" target="_blank">here</a>.</p>
           <button onClick={() => {window.location.href="/"}} className="glide-btn error-back-btn">back</button>
         </div>
       </div>
